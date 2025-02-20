aca7001ce228d22fe4e57bc6ea9525f554d2df6a
https://github.com/iniyanas-12 <iniyanas@yantravision.com>
2025-02-20 16:58:43 +0530

    included mul()

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 int add(int a, int b) { return a + b; }
 int sub(int a, int b) { return a - b; }
+int mul(int a, int b) { return a * b; }
 
 int main(){
    printf("Addition: %d\n", add(3, 4));
    printf("Subtraction: %d\n", sub(5, 2));
+   printf("Multiplication: %d\n", mul(5,2));
 }
