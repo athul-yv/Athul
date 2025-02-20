8bc04daef8ba3e84cd35672585e7e853dff82b32
madheshwaran-s <madheshwaran.s@yantravision.com>
2025-02-20 17:01:41 +0530

    added division function

--- main.c
+++ main.c
@@ -2,9 +2,11 @@
 int add(int a, int b) { return a + b; }
 int sub(int a, int b) { return a - b; }
 int mul(int a, int b) { return a * b; }
+int div(int a, int b) { return a / b; }
 
 int main(){
    printf("Addition: %d\n", add(3, 4));
    printf("Subtraction: %d\n", sub(5, 2));
    printf("Multiplication: %d\n", mul(5,2));
+   printf("Division: %d\n", div(10,2));
 }
