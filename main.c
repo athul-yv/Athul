c6c95b081c21421269d6656cf14b207e38415991
athul-yv <athul.krishna@yantravision.com>
2025-02-20 16:55:13 +0530

    included sub()

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-int add(int a, int b){ return a + b; }
+int add(int a, int b) { return a + b; }
+int sub(int a, int b) { return a - b; }
 
 int main(){
    printf("Addition: %d\n", add(3, 4));
+   printf("Subtraction: %d\n", sub(5, 2));
 }
